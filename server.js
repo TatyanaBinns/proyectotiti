41bdd9a9b68572c885a5c3e17615d6336e5e982c
Tatyana Binns <maslikova.tatyana@knights.ucf.edu>
2022-01-23 01:10:02 -0500

    add store ping and list pings

--- server.js
+++ server.js
@@ -41,6 +41,15 @@ async function dbInit(){
     //Debug function, gets the current time from the database server.
     dbApi.now = () => exec('SELECT NOW() as now');
     
+	dbApi.storeping = (trackerid, stationid, lat, lon, velocity) => exec('INSERT INTO public.pings ("timestamp", trackeruuid, stationuuid, "location", velocity) VALUES(now(), $1, $2, point($3, $4), $5);', [trackerid, stationid, lat, lon, velocity]);
+	
+	dbApi.ensureStation = (stationid) => exec("INSERT INTO public.basestations (name, location, description, stationuuid) VALUES('UnknownStation', point(28.600673933160543, -81.19702573512788), 'Unknown Base Station ID#$1', $1) ON CONFLICT DO NOTHING;", [stationid]);
+	
+	dbApi.ensureTracker = (trackerid) => exec("INSERT INTO public.trackers (animalid, trackeruuid) VALUES(-1, $1) ON CONFLICT DO NOTHING;", [trackerid]);
+	
+	dbApi.listpings = () => exec('SELECT * FROM pings;');
+	
+	
     console.log("Database API Loaded");
 }
 dbInit().catch(err => console.log(err));
@@ -50,9 +59,18 @@ dbInit().catch(err => console.log(err));
 
 const app = express();
 //============ Initialize endpoints ============
-app.get('/', (req, res) => (async() =>
+app.get('/', (req, res) => (async() => {
     res.send(JSON.stringify(await dbApi.now()))
-)());
+})());
+app.get('/storeping', (req, res) => (async() => {
+	var trackerid= 4242, stationid = 42, lat= 28.365349, lon=81.125664, velocity=0.39;
+	await dbApi.ensureStation(stationid);
+	await dbApi.ensureTracker(trackerid);
+    res.send(JSON.stringify(await dbApi.storeping(trackerid, stationid, lat, lon, velocity)))
+})());
+app.get('/listpings', (req, res) => (async() => {
+    res.send(JSON.stringify(await dbApi.listpings()))
+})());
 
 
 //====== Start listening on whatever port ======
