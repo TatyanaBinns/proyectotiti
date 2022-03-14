ed311c9f02150f4575915a3060ba99ef75469d3f
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-14 19:12:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ let uri = process.env.DATABASE_URL;
 if (uri == null || uri == "")
   uri = "postgres://postgres:password@localhost:5432/postgres"; //TODO set an agreed upon local development
 
+
 //===== Setup the database connection and access functions
 dbApi = { };
 async function dbInit(){
