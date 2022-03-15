8a4467ac521919f6efaedfec96f5109fb04cc67f
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-15 11:45:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const bodyParser =require('body-parser')
 const { Client } = require('pg');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
-const path = require('path'); 
 
 
 //===== Pull in environment variables from Heroku
@@ -14,6 +12,16 @@ if (port == null || port == "")
 let uri = process.env.DATABASE_URL;
 if (uri == null || uri == "")
   uri = "postgres://postgres:password@localhost:5432/postgres"; //TODO set an agreed upon local development
+
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('front-end/build'));
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
+  });
+}
 
 //===== Setup the database connection and access functions
 dbApi = { };
@@ -239,31 +247,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.text({
     type: "*/*"
 }));
-
-app.use(cors());
-app.use((req, res, next) => 
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
-
-if (process.env.NODE_ENV === 'production') 
-{
-  // Set static folder
-  app.use(express.static('front-end/build'));
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'));
-  });
-}
 
 
 //============ Initialize endpoints ============
