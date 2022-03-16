1b2f99b5305cd0df70e82d8a0a6c3cac7a075fe1
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-16 11:45:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser =require('body-parser')
 const { Client } = require('pg');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 
 //===== Pull in environment variables from Heroku
@@ -249,6 +250,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.text({
     type: "*/*"
 }));
+
+app.use(cors());
+app.use((req, res, next) => 
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
 
 //============ Initialize endpoints ============
