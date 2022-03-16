b35a470d4f699cc92019491c264985ef6b29db8d
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-16 11:16:19 -0400

    Update Signup.js

--- front-end/src/screens/Signup.js
+++ front-end/src/screens/Signup.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import {Box, Button, TextField,Typography} from '@material-ui/core';
 import React, { useState } from "react";
+import axios from "axios";
 
 
 function Signup() {
