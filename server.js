44322b42ddc2fef617fea1b38ddac3baa0a53ae9
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-18 11:36:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -230,14 +230,6 @@ const app = express();
 const path = require('path');
 
 
-if(process.env.NODE_ENV === 'production') 
-{  
-  app.use(express.static(path.join(__dirname, 'front-end/build')));  
-  app.get('*', (req, res) => {    
-    res.sendfile(path.join(__dirname = 'front-end/build/index.html'));  
-  })
-}
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: true 
