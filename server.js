2db7a6b7aeb627cccfc255ecabe8fff258458f8d
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-18 10:50:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -228,16 +228,6 @@ dbInit().catch(err => console.log(err));
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'front-end/build')));
-
-if(process.env.NODE_ENV === 'production') 
-{  
-  app.use(express.static(path.join(__dirname, 'front-end/build')));  
-  app.get('/', (req, res) => {    
-    res.sendfile(path.join(__dirname = 'front-end/build/index.html'));  
-  })
-}
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: true 
@@ -549,6 +539,16 @@ app.delete('/base-stations/:stationId', deleteBaseStation);
 
 //======= Ping Routes ======
 app.get('/pings/:trackerId?/:startTime?-:endTime?', getPings);
+
+app.use(express.static(path.join(__dirname, 'front-end/build')));
+
+if(process.env.NODE_ENV === 'production') 
+{  
+  app.use(express.static(path.join(__dirname, 'front-end/build')));  
+  app.get('*', (req, res) => {    
+    res.sendfile(path.join(__dirname = 'front-end/build/index.html'));  
+  })
+}
 
 
 //====== Helper Functions ======
