2ab3f3e715fafb5c8f92716722287c6dc9c62fb0
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-18 11:39:46 -0400

    Update Signup.js

--- front-end/src/screens/Signup.js
+++ front-end/src/screens/Signup.js
@@ -15,6 +15,7 @@ function Signup() {
   const submitForm = () => {
       axios.get(`https://proyectotiti.herokuapp.com/listpings`)
           .then(function (response) {
+            console.log("I am the newest version");
             console.log(response);
           })
   };
