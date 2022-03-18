511a44dbb86fe98436b000dacfee734dd6a9256e
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-18 10:23:33 -0400

    Update Signup.js

--- front-end/src/screens/Signup.js
+++ front-end/src/screens/Signup.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import {Box, Button, TextField,Typography} from '@material-ui/core';
 import React, { useState } from "react";
+import axios from "axios";
 
 
 function Signup() {
@@ -12,7 +13,10 @@ function Signup() {
   const [username, setUsername] = useState("");
 
   const submitForm = () => {
-      console.log("I changed!!!");
+      axios.get(`https://proyectotiti.herokuapp.com/`)
+          .then(function (response) {
+            console.log("response);
+          })
   };
   
   return (    
