5514d86062685590879b7bd3b3e70626087b8126
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-26 18:49:47 -0400

    Update Admin.js

--- front-end/src/screens/Admin.js
+++ front-end/src/screens/Admin.js
@@ -11,18 +11,32 @@ function Admin() {
   return (
     <div className="Settings-header">
       <Header/>
-      <div style={{ maxWidth: '100%' }}>
-        <MaterialTable
-          columns={[
-            { title: 'Ad', field: 'name' },
-            { title: 'Soyad', field: 'surname' },
-            { title: 'Doğum Y', field: 'birthYear', type: 'numeric' },
-            { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlurfa' } }
-          ]}
-          data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
-          title="Demo Title"
-        />
-      </div>
+      <div className="leaflet-container">
+      <MaterialTable
+        title="Simple Action Preview"
+        columns={[
+          { title: 'Name', field: 'name' },
+          { title: 'Surname', field: 'surname' },
+          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+         {
+           title: 'Birth Place',
+            field: 'birthCity',
+           lookup: { 34: 'İstanbul', 63: 'Şanlurfa' },
+          },
+        ]}
+       data={[
+          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+        ]}          
+        actions={[
+          {
+            icon: 'save',
+            tooltip: 'Save User',
+           onClick: (event, rowData) => alert("You saved " + rowData.name)
+          }
+        ]}
+      />
+    </div>
     </div>
   );
 }
