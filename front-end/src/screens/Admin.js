644ad5ede599fb28c28b81a2e849f70736b2f5a3
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-26 18:41:44 -0400

    Update Admin.js

--- front-end/src/screens/Admin.js
+++ front-end/src/screens/Admin.js
@@ -14,10 +14,10 @@ function Admin() {
       <div style={{ maxWidth: '100%' }}>
         <MaterialTable
           columns={[
-            { title: 'Adı', field: 'name' },
-            { title: 'Soyadı', field: 'surname' },
-            { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
-            { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' } }
+            { title: 'Ad', field: 'name' },
+            { title: 'Soyad', field: 'surname' },
+            { title: 'Doğum Y', field: 'birthYear', type: 'numeric' },
+            { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlurfa' } }
           ]}
           data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
           title="Demo Title"
