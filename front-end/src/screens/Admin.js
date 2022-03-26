261f13b6e85a58675c6caf5069ccc4f70dce0f37
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-26 18:59:42 -0400

    Admin table

--- front-end/src/screens/Admin.js
+++ front-end/src/screens/Admin.js
@@ -2,7 +2,7 @@ import {Box} from "@material-ui/core";
 import "./styles.css";
 import Header from "./Header";
 import React, { useEffect, useState } from "react";
-import MaterialTable from "material-table";
+import Table from "./Table";
 import axios from "axios";
 
 
@@ -11,32 +11,9 @@ function Admin() {
   return (
     <div className="Settings-header">
       <Header/>
-      <div className="leaflet-container">
-      <MaterialTable
-        title="Simple Action Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-         {
-           title: 'Birth Place',
-            field: 'birthCity',
-           lookup: { 34: 'İstanbul', 63: 'Şanlurfa' },
-          },
-        ]}
-       data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}          
-        actions={[
-          {
-            icon: 'save',
-            tooltip: 'Save User',
-           onClick: (event, rowData) => alert("You saved " + rowData.name)
-          }
-        ]}
-      />
-    </div>
+      <Box color="black" bgcolor="white" p={25} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+            <Table/>
+        </Box>
     </div>
   );
 }
---
+++ front-end/src/screens/Table.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import MaterialTable from 'material-table'
+
+export default function Table(){
+  
+    return (
+      <div style={{ maxWidth: '100%' }}>
+        <MaterialTable
+          columns={[
+            { title: 'Ad', field: 'name' },
+            { title: 'Soyad', field: 'surname' },
+            { title: 'Doğum Y', field: 'birthYear', type: 'numeric' },
+            { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlurfa' } }
+          ]}
+          data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
+          title="Demo Title"
+        />
+      </div>
+    );
+}
