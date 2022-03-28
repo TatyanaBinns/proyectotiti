77191db711f0e50109a23f66821f7d8d8630007d
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-28 13:00:10 -0400

    Update Login.js

--- front-end/src/screens/Login.js
+++ front-end/src/screens/Login.js
@@ -5,7 +5,6 @@ import {Link,useNavigate} from 'react-router-dom';
 import axios from "axios";
 
 function Login() {
-
     let navigate = useNavigate();
 
     const [username, setUsername] = useState("");
