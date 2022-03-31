1c96b598bddbee5cb646698f4d39bf94af8f8c2f
AlexS7898 <84539153+AlexS7898@users.noreply.github.com>
2022-03-31 18:23:09 -0400

    Update Home.js

--- front-end/src/screens/Home.js
+++ front-end/src/screens/Home.js
@@ -1,19 +1,64 @@
-import {Box, Button,Typography} from '@material-ui/core';
+import {Box,Typography} from '@material-ui/core';
 import "./styles.css";
 import Header from "./Header";
+import {DataGrid, GridToolbar} from "@mui/x-data-grid";
 
 
 
 function Home() {
+  const columns = [
+    { field: 'id', headerName: 'Tracker ID', width: 200 },
+  ];
+  
+  const rows = [
+    { id: 1},
+    { id: 2},
+    { id: 3},
+  ];
+
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const submitForm = () => {
+    console.log(selectedRows);
+  };
 
   return (
-    <div className="Home-header">
+    <div className="Settings-header">
       <Header/>
-        <Box color="black" bgcolor="white" p={8} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
-        <Typography variant="h5">
-            Welcome to the site {localStorage.getItem('username')}
-          </Typography>
-        </Box>
+      <div className="Table-container">
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+          autoHeight
+          components={{ Toolbar: GridToolbar }}
+          onSelectionModelChange={(ids) => {
+            const selectedIDs = new Set(ids);
+            const selectedRows = rows.filter((row) =>
+              selectedIDs.has(row.id),
+            );
+  
+            setSelectedRows(selectedRows);
+          }}
+        />
+        <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{
+              backgroundColor: "#299846",
+            }}
+            margin = "normal"
+            className="form-input"
+            size="large"
+            onClick={submitForm}
+          >
+            View Selected Tracker Data
+          </Button>  
+      </div>
     </div>
   );
 }
